986281ef292bc2fdc50e26cafc62af312f422f0b
hukening <carrieee2018@gmail.com>
2019-12-28 13:42:38 -0500

    Save before ejecting

--- Documents/develop/muke/recruitment-app/imooc/src/App.js
+++ Documents/develop/muke/recruitment-app/imooc/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createStore} from 'redux';
+
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload~.
         </p>
         <a
           className="App-link"
